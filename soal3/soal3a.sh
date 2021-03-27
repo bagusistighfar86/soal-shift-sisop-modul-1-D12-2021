1c0c10bd344ab9ab7a61fcca4a0db226a8bbe082
bagusistighfar86 <79921639+bagusistighfar86@users.noreply.github.com>
2021-03-27 20:35:20 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-#loop
-i=1; 
-k=1;
 for ((i=1; i<24; i=i+1))
 do
     echo -e "i=$i\n"
-    if [[ $k -lt 10 ]]; then
-        wget -O "Koleksi_0$k.jpg" -o ->> Foto.log https://loremflickr.com/320/240/kitten
-    else
-        wget -O "Koleksi_$k.jpg" -o ->> Foto.log https://loremflickr.com/320/240/kitten
-    fi
+    wget -O "Kitten_$i.jpg" -o ->> Foto.log https://loremflickr.com/320/240/kitten
+done
 
-    files="$( find -type f )"
+files="$( find -type f )"
     for file1 in $files; do
         for file2 in $files; do
-            # echo "checking $file1 and $file2"
+            # echo "cek $file1 and $file2"
             if [[ "$file1" != "$file2" && -e "$file1" && -e "$file2" ]]; then
                 if diff "$file1" "$file2" > /dev/null; then
-                    # echo "$file1 and $file2 are duplicates"
+                    #echo "$file1 dan $file2 duplikat"
                     rm -v "$file2"
                     k=$[$k-1]
                 fi
             fi
         done
     done
-    k=$[$k+1]
+
+j=1
+for file in *.jpg; do
+    if [[ $j -lt 10 ]]; then
+        mv "$file" "Koleksi_0$j.jpg"
+    else
+        mv "$file" "Koleksi_$j.jpg"
+    fi
+    j=$[$j+1]
 done
